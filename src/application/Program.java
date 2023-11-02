c52cea881de05ee9a071700a6e2faeef9d0d8f04
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-02 14:30:21 -0300

    Departament entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import model.entities.Departament;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-
+		Departament dep = new Departament(1, "books");
+		
+		System.out.println(dep);
 	}
 
 }
