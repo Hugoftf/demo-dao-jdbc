b62a7b57fe806ee5ce75e6ba2fc4c49bbb0c31de
Hugo de França <f3rreirahugo@hotmail.com>
2023-11-02 21:42:40 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,16 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
+		
+		
+		System.out.println("\n===TEST 5: seller findAll	 ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
+	
+	
 
 }
